032ddb088d0918aa1c721240ec34485031ba8a9a
Herman Obst Demaestri <herman.obst@lambdaclass.com>
2021-02-23 17:26:53 -0300

    grammar fixes

--- 01_science_technology_and_epistemology/01_science_technology_and_epistemology.jl
+++ 01_science_technology_and_epistemology/01_science_technology_and_epistemology.jl
@@ -8,14 +8,14 @@ using InteractiveUtils
 md"""
 ## The difference between Science and Technology
 
-Anyone would agree that science and technology have a lot in common but, what are the essential differences between them? and how do they interact between each other? 
+Anyone would agree that science and technology have a lot in common, but what are the essential differences between them? and how do they interact between each other? 
 These are fundamental questions for us, and ones that most people don't have a clear answer for. 
 This leads to misconceptions about key aspects on, for example, how knowledge is created and, more generally, how the world operates. 
 
 One fundamental difference between the two is that while science tries to understand the physical world in which we live, technology aims to transform it. 
 So, technology has a strong practical goal and science a more intellectual one. 
 A very frequent misunderstanding that is nowadays very much encouraged by academia is to think that first, one needs to acquire all of the theoretical and formal knowledge about a subject matter before being able to apply it and transform reality. 
-But in reality this is not the case. The process occurs, in most cases, in the opposite way: One lives in reality, interacts with it, takes action, makes mistakes, tries again, and in that way slowly discovers how the world works. 
+But in reality this is not the case. The process occurs, in most cases, in the opposite way: one lives in reality, interacts with it, takes action, makes mistakes, tries again, and in that way slowly discovers how the world works. 
 And then, of course, it is useful to formalize our knowledge in order to be able to transmit that learning, lay solid foundations and to continue advancing in our understanding.
 
 ## What is technology?
@@ -33,11 +33,11 @@ Is it necessary first to have an absolute understanding of the process we are tr
 Or is technology created in a more chaotic process, through trial and error?
 
 So let's think for a moment, does a child need to know the gear mechanisms of a bicycle in order to be able to use it? Does Lionel Messi need to know about fluid dynamics to make the ball take a curved trajectory? Did the Romans need to know the Navier-Stokes equation in order to build their huge aqueducts? 
-Maybe you are thinking that these are very specific cases, but the reality is that those example share a general and universal property: The knowledge is acquired based on experimentation, implementation and heuristics, pure trial and error and with much less theoretical knowledge than many believe. 
+Maybe you are thinking that these are very specific cases, but the reality is that those examples share a general and universal property: the knowledge is acquired based on experimentation, implementation and heuristics, pure trial and error and with much less theoretical knowledge than many believe. 
 And technological innovation tends to follows the same mechanism, it was discovered by people that were taking risks, taking action, with a pure trial-and-error experiential manner and, above all, without anyone truly understanding the theory behind.
 
 The current understanding of how knowledge is produced does not match how it actually happens, and may even work against our society's development.
-Nowadays, there is a kind of linear thinking model in which it is taken for granted that the process of knowledge production has a very specific way of occurring: First, academic and theoretical discoveries are made. And only then, with all of that formalization achieve, one can start thinking of putting it into practical use.
+Nowadays, there is a kind of linear thinking model in which it is taken for granted that the process of knowledge production has a very specific way of occurring: first, academic and theoretical discoveries are made. And only then, with all of that formalization achieve, one can start thinking of putting it into practical use.
 This type of thinking is counterproductive because, being so widespread, it causes many people to be more concerned with constantly acquiring theoretical knowledge, rather than taking action and immersing themselves in practice.
 
 By looking at the history of technology and innovation a repeating pattern can be seen: there is a constant differentiation between the people who do and the people who watch and then narrate. 
@@ -62,7 +62,7 @@ So makers start original, and get good, and scientists start good, and get origi
 
 So, what role does science play in all this and why is it useful for technology? As we already said, it is a tool that technology has to formalize all that chaotic based knowledge discovery. 
 Formalization is very useful to teach and communicate some base knowledge and to build a solid foundation in order to keep expanding it. 
-But it is also the way we find to eliminate subjectivities regarding the understanding of our world. 
+But it is also the way we found to eliminate subjectivities regarding the understanding of our world. 
 Thanks to science, it is no longer important what we think about reality, but rather the experimentation and the consequent testing of the hypotheses we made. 
 
 ## What is Science?
@@ -82,7 +82,7 @@ It’s the opposite of looking for verification; you must try to show the theory
 
 So, science is a method we have built to conclude that hypotheses we propose are not false. 
 This doesn't mean they are true, and this is one of the core concepts of the scientific method. There are no dogmas in science. 
-Scientific propositions, claims, hypotheses or theories **must** be able to be tested to try to falsify it. 
+Scientific propositions, claims, hypotheses or theories must be able to be tested to try to falsify it. 
 If there isn't a way to test their validity, then they aren't scientific propositions, and it is not a matter of science to discuss such statements. 
 Rather, we can debate them in the realms of philosophy and other disciplines. 
 Science happens in the context of a community; it makes no sense to talk about an hypothesis being false by its own, there must be a community that can validate and replicate the evidence. 
@@ -106,7 +106,7 @@ In this way, it is quite natural to think of beautiful equations being derived t
 But this concept of mathematical beauty can sometimes be taken too far, seeking and trying to impose it to all scientific discovery when in reality it is a human construct.
 
 And there are fields of science apart from physics, like biology or chemistry, that are examples of fields that don't have any of these "beautiful" and extremely precise equations. 
-In the rest of science, we have what are called "emergent laws" that arise from very complicated systems wherewhere the individual details don't matter, but the system as a whole behave in a very specific and recognizable way.
+In the rest of science, we have what are called "emergent laws" that arise from very complicated systems where the individual details don't matter, but the system as a whole behave in a very specific and recognizable way.
 
 So, there is a clear relationship between Science, technology and Math. But why is the last one so important?
 
@@ -119,7 +119,7 @@ The first steps humanity made into the mathematical world were done to communica
 If we ask you what the simplest form of mathematics that comes to your mind is, you probably are going to think about counting. And essentially there is where everything started. 
 To our minds, that are now accustomed to very complex ideas of all kinds, such as the internet or the economy, maybe counting doesn't sound like a very phenomenal idea. 
 But think about the conceptual leap our ancestors had to make in order to arrive to an abstract construct such as a number. 
-After all, what is a number? What does it look like? What comes to your mind when I speak about the number two? What do two cats and two roses have in common? Numbers appeared as a way to have a more efficient communication. 
+After all, what is a number? What does it look like? What comes to your mind when we speak about the number two? What do two cats and two roses have in common? Numbers appeared as a way to have a more efficient communication. 
 Steven Strogatz refers, in his book The Joy of x, to a particular episode of the Sesame Street show, '123 Count with Me'. 
 Although it may sound silly and childish, it makes a great metaphor of the usefulness of numbers. 
 In the show, one of the characters, Humphrey, is working as a waiter in a restaurant and he takes an order of some penguins. 
@@ -135,18 +135,18 @@ With variables, the conceptualization of magnitudes that change their value duri
 Moreover, functions were the key abstraction tool to represent how variables depend on other variables, and the concept of independent and dependent variables appears.
 
 For many years, the desire to understand the world and postulate physical laws motivated mathematical discovery. 
-This is really easy to understand with the creation of Calculus by Leibnitz and Newton, one of the most important mathematical tools we have in our arsenal, which has today extended use along science, engineering, economy and many more fields. 
-Alongside Calculus, another important and fundamental mathematical field started being conceptualized and developed. 
+This is really easy to understand with the creation of calculus by Leibnitz and Newton, one of the most important mathematical tools we have in our arsenal, which has today extended use along science, engineering, economy and many more fields. 
+Alongside calculus, another important and fundamental mathematical field started being conceptualized and developed. 
 With the stimulus of games of chance and gambling, the mathematical theory of probability was born by Pascal and Fermat. 
-Almost in paralell, Statistics started as an applied field dealing with data from states, such as population demographics and economy, but it slowly growed and extended to the collection of any kind of data, its analysis, interpretation and the extraction of conclusions from it. 
-The evolution process of Statistics was intimately related with the development of probability, and, with the Theory of errors, ultimately all three of these mathematical disciplines, Calculus, Probability and Statistics, played a fundamental role in hypothesis testing and scientific research.
+Almost in paralell, statistics started as an applied field dealing with data from states, such as population demographics and economy, but it slowly growed and extended to the collection of any kind of data, its analysis, interpretation and the extraction of conclusions from it. 
+The evolution process of statistics was intimately related with the development of probability, and, with the theory of errors, ultimately all three of these mathematical disciplines, calculus, probability and statistics, played a fundamental role in hypothesis testing and scientific research.
 
 As systems under study started to grow in number of components and the interactions between them were taken into account, the modelling techniques that were most frequently used started to show some lacking capabilities to describe these systems. 
-A new conceptual approach revolutionized physics, and that is Statistical Mechanics. 
-Instead of having to know the exact state of a system conformed by a big number of elements that interact in complicated ways, Statistical Mechanics relies on probability and statistical methods to characterize the behaviour of the system. 
+A new conceptual approach revolutionized physics, and that is statistical mechanics. 
+Instead of having to know the exact state of a system conformed by a big number of elements that interact in complicated ways, statistical mechanics relies on probability and statistical methods to characterize the behaviour of the system. 
 This field settled a framework to think about systems in a different way. 
 The impossibility of knowing everything is a fact that is embraced in this line of thought and properly handled. 
-Many more of these fresh ideas kept developing along the years, such as Complexity Theory, a field that focuses on emergent properties of systems of aggregates of many parts. 
+Many more of these fresh ideas kept developing along the years, such as complexity theory, a field that focuses on emergent properties of systems of aggregates of many parts. 
 
 ## The emergence of Data Science
 
@@ -159,16 +159,16 @@ We are living in the so-called "Petabyte Age".
 At this scale, information is not a matter of simple three- and four-dimensional taxonomy and order but of dimensionally agnostic statistics.
 And this is so important because is expanding our possibilities to do science. 
 In the complex, messy domains that we already mention, particularly game-theoretic domains involving unpredictable agents such as human beings, there are no general theories that can be expressed in simple equations like $F = ma$ or $E = m c^2$. 
-But now we have massive amounts of available data describing those complex systems, so employing non-parametric density approximation models such as nearest-neighbors or kernel methods rather than parametric models such as low-dimensional linear regression may be the solution to gain useful insights. Theory is expanding into new forms. A new form where, if it allows to solve problems, correlation is enough.
+But now we have massive amounts of available data describing those complex systems, so employing non-parametric density approximation models such as nearest-neighbors or kernel methods rather than parametric models such as low-dimensional linear regression may be the solution to gain useful insights. Theory is expanding into new forms. A new form where, if it allows to solve problems, correlation of variables is enough.
 
-The exponential progress that we have been seeing in computer technology is a key aspect of Data Science. 
-So much so that it is common to hear that Data Science is the conjunction between Statistics and Computer Science and, undoubtedly, there is a lot of truth in this. 
-Okay, but, what is the real difference between Data science and Statistics?
+The exponential progress that we have been seeing in computer technology is a key aspect of data science. 
+So much so that it is common to hear that Data Science is the conjunction between statistics and computer science and, undoubtedly, there is a lot of truth in this. 
+But, what is the real difference between data science and statistics?
 
 Well, the key difference is the (almost) pure practical point of view. 
 In the data science field the ultimate goal is to solve a huge diversity of problems and also creating a replicable solution. 
 In contrast to statistics where you can define the data that you are going to use, here the data itself became the object of study. 
-And, as the world it's complicated, the data we encounter too. 
+And, as the world its complicated, the data we encounter too. 
 In data science we make use of data collection, management, and presentation, to focus more on predicting future outcomes and less on merely inferring relationships. 
 So the motivation always will be to take action on the insights that you learn.
 
@@ -176,7 +176,7 @@ Apart from the particular application of programming in this field, we believe t
 Unlike academia, nobody cares in the informatics world if you have a PhD. What really matters is the quality of your code and your methods. 
 The same is true here. 
 Here we will not show polished and definitive solutions, but we will attack a wide variety of topics, showing several heuristics that will serve for anyone to build solid foundations in this field that we love so much. 
-Here we don´t care about academic degrees, only the ingenuity to engineer and solve the most challenging problems.
+Here we don't care about academic degrees, only the ingenuity to engineer and solve the most challenging problems.
 
 ## Our philosophy: The right balance between practice and theory
 
@@ -185,7 +185,7 @@ And it is to propose interesting and complex problems, to think about how to sol
 Our goal is to show the hardcore academic that it doesn't hurt to take a few blind steps to attack real problems and to show the purely practical person the benefits of having a deeper understanding of modelling, probability and statistics beyond just being able to use a few libraries.
 
 We will be tackling a lot of different practical problems, emphasizing the interdisciplinary approach of the subject. 
-Physics, Economy and Sports are just some of the topics discussed in these chapters. Data Science can't be done without the help of a high-level programming language. 
+Physics, Economy and Sports are just some of the topics discussed in these chapters. data science can't be done without the help of a high-level programming language. 
 This is an essential skill, just as math and a scientific mindset. 
 In this book, the programming tool we are going to use is Julia, a language engineered from its roots for scientific applications, in particular, Data Science.
 
