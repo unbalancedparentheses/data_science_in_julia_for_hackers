129fd3ce1d607aa788298511f0cf479902d31c3b
Pedro Fontana <pedro.fontana@lambdaclass.com>
2021-04-12 13:47:09 -0300

    made corrections, html updated

--- 05_prob_prog_intro/05_prob_prog_intro.jl
+++ 05_prob_prog_intro/05_prob_prog_intro.jl
@@ -27,53 +27,54 @@ end
 # ╔═╡ 124ab9da-1ae4-11eb-1c1a-a3b9ac960130
 using Turing
 
-# ╔═╡ 9e8252ac-1af6-11eb-3e19-ddd43d9cd1a9
-begin
-	using StatsPlots
-	
-	plot(Beta(2,2), fill=(0, .5,:dodgerblue), ylim=(0,2), legend=false, size=(450, 300))
-	xlabel!("p")
-	ylabel!("Probability")
-	title!("Prior distribution for p")
-end
-
 # ╔═╡ 9608da48-1acd-11eb-102a-27fbac1ec88c
 md" # Probabilistic Programming"
 
 # ╔═╡ 14b8e170-1ad9-11eb-2111-3784e7029ba0
 md"
-In the previous chapter we introduced some of the basic mathematical tools we are going to make use through the book. We talked about histograms, probability, probability distributions and the Bayesian way of thinking.
+In the previous chapters we introduced some of the basic mathematical tools we are going to make use of throughout the book. We talked about histograms, probability, probability distributions and the Bayesian way of thinking.
 
-We will start this chapter discussing the fundamentals of another useful tool, that is, *Probabilisti Programming*, and more specifically, *Probabilistic Programming Languages* or PPL's. These are systems, usually embedded inside some programming language, that are designed for building and reasoning about Bayesian models. They offer scientists an easy way to define probability models and solving them automatically. 
 
-In Julia, there are a few PPL's being developed, and we will be using two of them, *Turing.jl* and *Soss.jl*. We will be focusing in some examples to explain the general approach when using this tools.  
+
+We will start this chapter by discussing the fundamentals of another useful tool, that is, probabilistic programming, and more specifically, how to apply it using probabilistic programming languages or PPLs.
+These are systems, usually embedded inside a programming language, that are designed for building and reasoning about Bayesian models. They offer scientists an easy way to define probability models and solving them automatically. 
+
+In Julia, there are a few PPLs being developed, and we will be using two of them, Turing.jl and Soss.jl. We will be focusing on some examples to explain the general approach when using this tools.  
+
 "
 
 # ╔═╡ e8557342-1adc-11eb-3978-33880f4a97a2
 md" #### Coin flipping example"
 
 # ╔═╡ babb3d32-1adb-11eb-2f52-7799a2031c2b
-md" We are going now to tackle a well known example, just to settle some ideas: flipping a coin. But this time, from a Bayesian perspective.
+md" Let's revisit the old example of flipping a coin, but from a Bayesian perspective, as a way to lay down some ideas.
 
 So the problem goes like this:
 Suppose we flip a coin N times, and we ask ourselves some questions like:
 - Is getting heads as likely as getting tails?
 - Is our coin biased, preferring one output over the other?
 
-To answer these questions we are going to build a simple model, with the help of our probabilistic programming languages in Julia.
+To answer these questions we are going to build a simple model, with the help of Julia libraries that add PPL capabilities.
 
-Let's start thinking in a Bayesian way. The first thing we should ask ourselves is: Do we have any *prior* information about the problem? Since the plausibility of getting heads is formally a probability (let's call it *$p$*), we know it must lay between 0 and 1. 
+Let's start thinking in a Bayesian way. The first thing we should ask ourselves is: Do we have any prior information about the problem? Since the plausibility of getting heads is formally a probability (let's call it $p$), we know it must lay between 0 and 1. 
 Do we know anything more?
 Let's skip that question for the moment and suppose we don't know anything more about $p$. This total uncertainty is also some kind of information we can incorporate in our model. 
 How?  	
-Because we can assign equal probability for each value of $p$ between 0 and 1, while assigning 0 probability for the remaing values. This just means we don't know anything and that every outcome is equally possible. Translating this into a probability distribution, it means that we are going to use a *Uniform* prior distribution for $p$, and the domain of this function will be between 0 and 1. 
+Because we can assign equal probability for each value of $p$ between 0 and 1, while assigning 0 probability for the remaing values. This just means we don't know anything and that every outcome is equally possible. Translating this into a probability distribution, it means that we are going to use a Uniform prior distribution for $p$, and the domain of this function will be between 0 and 1. 
+
+Do we know anything else? 
+Let's skip that question for the moment and suppose we don't know anything else about *p*. 
+This complete uncertainty also constitutes information we can incorporate into our model. 
+How so? 
+Because we can assign equal probability to each value of *p* while assigning 0 probability to the remaining values. 
+This just means we don't know anything and that every outcome is equally likely. Translating this into a probability distribution, it means that we are going to use a uniform prior distribution for *p*, and the function domain will be all numbers between 0 and 1.
 "
 
 # ╔═╡ 10705938-1af3-11eb-27f4-a96a49a43a67
 md"
 So how do we model the outcomes of flipping a coin?
 
-Well, if we search for some similar type of experiment, we find that all processes in which we have two possible outcomes –heads or tails in our case–, and some probability  *p* of success –probability of heads–, these are called *Bernoulli* trials. The experiment of performing a number $N$ of Bernoulli trials, gives us the so called *Binomial distribution*. For a fixed value of $N$ and $p$, the Bernoulli distribution gives us the probability of obtaining different number of heads (and tails too, if we know the total number of trials and the number of times we got heads, we know that the remaining number of times we got tails). Here, $N$ and $p$ are the *parameters* of our distribution.
+Well, if we search for some similar type of experiment, we find that all processes in which we have two possible outcomes –heads or tails in our case–, and some probability  *p* of success –probability of heads–, these are called Bernoulli trials. The experiment of performing a number $N$ of Bernoulli trials, gives us the so called Binomial distribution. For a fixed value of $N$ and $p$, the Bernoulli distribution gives us the probability of obtaining different number of heads (and tails too, if we know the total number of trials and the number of times we got heads, we know that the remaining number of times we got tails). Here, $N$ and $p$ are the parameters of our distribution.
 "
 
 # ╔═╡ 1ec4a39e-1af3-11eb-29e3-5d78e4c4613a
@@ -83,17 +84,20 @@ Well, if we search for some similar type of experiment, we find that all process
 @bind p html"<input type=range min=0.1 max=1.0 step=0.001>"
 
 # ╔═╡ 69ad2e2e-1af3-11eb-09e2-bd8e04190c9d
-bar(Binomial(N,p), xlim=(0, 90), label=false, xlabel="Succeses", ylabel="Probability", title="Binomial distribution", color="green", alpha=0.8, size=(500, 350)) 
+begin
+	using StatsPlots
+	bar(Binomial(N,p), xlim=(0, 90), label=false, xlabel="Succeses", ylabel="Probability", title="Binomial distribution", color="green", alpha=0.8, size=(500, 350)) 
+end
 
 # ╔═╡ 889c04e2-1ae2-11eb-34c8-7b5a98d73676
 md"
 So, as we said, we are going to assume that our data (the count number of heads) is generated by a Binomial distribution. Here, $N$ will be something we know. We control how we are going to make our experiment, and because of this, we fix this parameter. The question now is: what happens with $p$? Well, this is actually what we want to estimate! The only thing we know until now is that it is some value from 0 to 1, and every value in that range is equally likely to apply.
 
-When we perform our experiment, the outcomes will be registered and in conjunction with the Bernoulli distribution we proposed as a generator of our data, we will get our *Likelihood* function. This function just tells us, given some chosen value of $p$, how likely it is that our data is generated by the Bernoulli distribution with that value of $p$. How do we choose $p$? Well, actually we don't. You just let randomness make it's choice. But there exist multiple types of randomness, and this is where our prior distribution makes its play. We let her make the decision, depending on it's particular randomness flavor. Computing the value of this likelihood function for a big number of $p$ samples taken from our prior distribution, gives us the posterior distribution of $p$ given our data. This is called **sampling**, and it is a very important concept in Bayesian statistics and probabilistic programming, as it is one of the fundamental tools that makes all the magic under the hood. It is the method that actually lets us update our beliefs. The general family of algorithms that follow the steps we have mentioned are named Markov Chain Monte Carlo (MCMC) algorithms. The computing complexity of this algorithms can get very high as the complexity of the model increases, so there is a lot of research being done to find intelligent ways of sampling to compute posterior distributions in a more efficient manner.
+When we perform our experiment, the outcomes will be registered and in conjunction with the Bernoulli distribution we proposed as a generator of our data, we will get our Likelihood function. This function just tells us, given some chosen value of $p$, how likely it is that our data is generated by the Bernoulli distribution with that value of $p$. How do we choose $p$? Well, actually we don't. You just let randomness make it's choice. But there exist multiple types of randomness, and this is where our prior distribution makes its play. We let her make the decision, depending on it's particular randomness flavor. Computing the value of this likelihood function for a big number of $p$ samples taken from our prior distribution, gives us the posterior distribution of $p$ given our data. This is called sampling, and it is a very important concept in Bayesian statistics and probabilistic programming, as it is one of the fundamental tools that makes all the magic under the hood. It is the method that actually lets us update our beliefs. The general family of algorithms that follow the steps we have mentioned are named Markov Chain Monte Carlo (MCMC) algorithms. The computing complexity of this algorithms can get very high as the complexity of the model increases, so there is a lot of research being done to find intelligent ways of sampling to compute posterior distributions in a more efficient manner.
 "
 
 # ╔═╡ 102b4be2-1ae4-11eb-049d-470a33703b49
-md"The model *coinflip* is shown below. It is implemented using the Turing.jl library, which will be handling all the details about the relationship between the variables of our model, our data and the sampling and computing. To define a model we use the macro *@model* previous to a function definition as we have already done. The argument that this function will recieve is the data from our experiment. Inside this function, we must write the explicit relationship of the all the variables involved in a logical way.
+md"The model coinflip is shown below. It is implemented using the Turing.jl library, which will be handling all the details about the relationship between the variables of our model, our data and the sampling and computing. To define a model we use the macro `@model` previous to a function definition as we have already done. The argument that this function will recieve is the data from our experiment. Inside this function, we must write the explicit relationship of the all the variables involved in a logical way.
 Stochastic variables –variables that are obtained randomly, following a probability distribution–, are defined with a '~' symbol, while deterministic variables –variables that are defined deterministically by other variables–, are defined with a '=' symbol.
 "
 
@@ -115,7 +119,7 @@ end
 
 # ╔═╡ cbe1d1f2-1af4-11eb-0be3-b1a02280acf9
 md"
-*coinflip* receives the *N* outcomes of our flips, an array of lenght *N* with 0 or 1 values, 0 values indicating tails and 1 indicating heads. 
+coinflip receives the N outcomes of our flips, an array of lenght N with 0 or 1 values, 0 values indicating tails and 1 indicating heads. 
 The idea is that with each new value of outcome, the model will be updating its believes about the parameter *p* and this is what the for loop is doing: we are saying that each outcome comes from a Bernoulli distribution with a parameter *p*, a success probability, shared for all the outcomes.
 
 Suppose we have run the experiment 10 times and had the outcomes:
@@ -142,11 +146,11 @@ end;
 
 # ╔═╡ fa37b106-1af5-11eb-11df-97aada833767
 md"
-So now we plot below the posterior distribution of p after our model updated, seeing just the first outcome, a 0 value or a tail. 
+So now we plot below the posterior distribution of *p* after our model updated, seeing just the first outcome, a 0 value or a tail. 
 
-How this single outcome have affected our beliefs about p?
+How this single outcome have affected our beliefs about *p*?
 
-We can see in the plot below, showing the posterior or updated distribution of *p*, that the values of *p* near to 0 have more probability than before, recalling that all values had the same probability, which makes sense if all our model has seen is a faliure, so it lowers the probability for values of p that suggest high rates of success.
+We can see in the plot below, showing the posterior or updated distribution of *p*, that the values of *p* near to 0 have more probability than before, recalling that all values had the same probability, which makes sense if all our model has seen is a faliure, so it lowers the probability for values of *p* that suggest high rates of success.
 "
 
 # ╔═╡ 0c570210-1af6-11eb-1d5d-5f78f2a000fd
@@ -187,6 +191,16 @@ md"What if we wanted to include more previous knowledge about the success rate *
 
 Let's say we know that the value of *p* is near 0.5 but we are not so sure about the exact value, and we want the model to find the plausibility for the values of *p*. Then including this knowledge, our prior distribution for *p* will have higher probability for values near 0.5, and low probability for values near 0 or 1. Seaching again in our repertoire of distributions, one that fulfill our wishes is a Beta distribution with parameters α=2 and β=2. It is ploted below." 
 
+# ╔═╡ 9e8252ac-1af6-11eb-3e19-ddd43d9cd1a9
+begin
+	
+	
+	plot(Beta(2,2), fill=(0, .5,:dodgerblue), ylim=(0,2), legend=false, size=(450, 300))
+	xlabel!("p")
+	ylabel!("Probability")
+	title!("Prior distribution for p")
+end
+
 # ╔═╡ d0f945f6-1af6-11eb-1f99-e79e7de8af80
 md"Now we define again our model just changing the distribution for *p*, as shown:"
 
@@ -207,7 +221,7 @@ begin
 end
 
 # ╔═╡ dc35ef50-1af6-11eb-2f77-d10aee2744dd
-md"Running the new model and plotting the posterior distributions, again adding one observation at a time, we see that with less examples we have a better approximations for the value of p."
+md"Running the new model and plotting the posterior distributions, again adding one observation at a time, we see that with less examples we have a better approximations for the value of *p*."
 
 # ╔═╡ b924b40a-1af7-11eb-13a9-0368b23ab0a4
 begin
